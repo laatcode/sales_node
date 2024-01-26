7527e5b8fcbbfef521a81cb8ec97c678bba8cfc5
Luis Angel Avila Torres <luisangelavilatorres@gmail.com>
2024-01-25 20:25:11 -0500

    Implementing server with express

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('Hello, World!')
+const express = require('express')
+const app = express()
+const PORT = 5000
+
+app.get('/', (req, res) => res.send('Hello, World!'))
+
+app.listen(PORT, () => console.log(`Server runing at port ${PORT}`))
