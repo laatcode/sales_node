90507bdec33e2828cf5f98f5d1f57643294015ea
Luis Angel Avila Torres <luisangelavilatorres@gmail.com>
2024-03-29 22:34:29 -0500

    Implementing cors library

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const PORT = 5000
 
 const routes = require('./routes')
 const errorHandler = require('./middlewares/errorHandler')
 
+app.use(cors())
 app.get('/', (req, res) => res.send('Hello, World!'))
 app.use(express.json())
 
