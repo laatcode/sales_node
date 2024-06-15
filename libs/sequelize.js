6634902935fe57798952c61a4128a82fac9ff791
Luis Angel Avila Torres <luisangelavilatorres@gmail.com>
2024-06-15 10:45:00 -0500

    Changing DB engine to MySQL

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -5,10 +5,10 @@ const {Sequelize} = require('sequelize')
 
 const USER = encodeURIComponent(process.env.DB_USER)
 const PASSWORD = encodeURIComponent(process.env.DB_PASS)
-const URI = `postgres://${USER}:${PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+const URI = `mysql://${USER}:${PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
 
 const sequelize = new Sequelize(URI, {
-    dialect: 'postgres' // Permite indicar la base de datos a conectar
+    dialect: 'mysql' // Permite indicar la base de datos a conectar
 })
 
 setupModels(sequelize) //Relaciona la conexión con los modelos definidos
