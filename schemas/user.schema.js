3429bd99b351acaca96ec636037c33f7c35a559b
Luis Angel Avila Torres <luisangelavilatorres@gmail.com>
2024-06-15 16:06:28 -0500

    Creating migration to add role column to users table

--- schemas/user.schema.js
+++ schemas/user.schema.js
@@ -3,15 +3,18 @@ const Joi = require('joi')
 const id = Joi.number().integer()
 const email = Joi.string().email()
 const password = Joi.string().min(6)
+const role = Joi.string().min(5)
 
 const createUserSchema = Joi.object({
     email: email.required(),
-    password: password.required()
+    password: password.required(),
+    role: role.required()
 })
 
 const updateUserSchema = Joi.object({
     email,
-    password
+    password,
+    role
 })
 
 const getUserSchema = Joi.object({
