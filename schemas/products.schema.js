1e0bee7af3a40d12e8f6bba02c92c4712a7e5312
Luis Angel Avila Torres <luisangelavilatorres@gmail.com>
2024-06-17 18:00:43 -0500

    Fixing product schema validation and creating category schema validation

--- schemas/products.schema.js
+++ schemas/products.schema.js
@@ -2,16 +2,22 @@ const Joi = require('joi')
 
 const id = Joi.string().uuid()
 const name = Joi.string().min(3).max(30)
+const description = Joi.string().min(10)
 const price = Joi.number()
+const image = Joi.string().uri()
 
 const createProductSchema = Joi.object({
     name: name.required(),
-    price: price.required()
+    description: description.required(),
+    price: price.required(),
+    image: image.required()
 })
 
 const updateProductSchema = Joi.object({
-    name: name,
-    price: price
+    name,
+    description,
+    price,
+    image
 })
 
 const getProductSchema = Joi.object({
